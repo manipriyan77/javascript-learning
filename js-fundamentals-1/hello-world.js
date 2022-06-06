fdb89bbb3525383148e567e587c7d8687509ac02
manipriyan77 <manipriyangopalan@gmail.com>
2022-06-06 13:43:32 +0530

    finished datatypes in fundamentals 1

---
+++ js-fundamentals-1/hello-world.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+
+// Values and variables
+let firstName = "Manipriyan";
+firstName = "Hari";
+
+// Variable name can't start with number,  special character and special character like & ! aren't allowed in varibale naming
+// Can't use reserved keywords and don't use variable name as "name"
+
+// typeof
+
+console.log(typeof "Mani");
