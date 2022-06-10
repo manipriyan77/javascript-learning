68ee96dac7cdd2ffdeac29d2c2fad227dfe86984
Manipriyan <manipriyan@6storage.com>
2022-06-11 01:15:03 +0530

    finished while loop

--- js-fundamentals-2/for-loop.js
+++ js-fundamentals-2/for-loop.js
@@ -11,21 +11,59 @@
 // console.log("Lifiting weights reps 1 🦾")
 // console.log("Lifiting weights reps 1 🦾")
 
-
+/*
 for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifiting weights reps ${rep} 🦾`)
 
 }
 
-const myInfo = [
-    "Manipriyan",
-    "Gopalan",
-    23,
-    "Developer",
-    ["Nishanth", "Karthi", "Maha"]
-]
+const myInfo = ["Manipriyan", "Gopalan", 23, "Developer", ["Nishanth", "Karthi", "Maha"]]
+
+const types = []
 
 console.log(myInfo.length)
 for (let i = 0; i < myInfo.length; i++) {
+    // reading an array
     console.log(myInfo[i], typeof myInfo[i])
+    // Filling the types of array
+    // types[i] = typeof myInfo[i]
+    types.push(typeof myInfo[i])
+}
+
+console.log(types)
+
+const years = [1991, 1999, 1996, 2007]
+const ages = []
+// Using loop
+for (let i = 0; i < years.length; i++) {
+    // ages[i] = 2022 - years[i]
+    ages.push(2022 - years[i])
+}
+console.log(ages)
+
+// Continue and break
+// continue- to break current iteration of the loop and continue to the next one
+// break is used to completely terminate the loop
+console.log("continue statement")
+for (let i = 0; i < myInfo.length; i++) {
+    if (typeof myInfo[i] !== "string") continue;
+    console.log(myInfo[i], typeof myInfo[i])
+}
+console.log("Break statement")
+for (let i = 0; i < myInfo.length; i++) {
+    if (typeof myInfo[i] === "number") break;
+    console.log(myInfo[i], typeof myInfo[i])
+}
+*/
+const myInfo = ["Manipriyan", "Gopalan", 23, "Developer", ["Nishanth", "Karthi", "Maha"], true]
+
+for (let i = myInfo.length - 1; i >= 0; i--) {
+    console.log(i, myInfo[i])
+}
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`----Strating Exersie ${exercise}`)
+    for (let reps = 1; reps <= 5; reps++) {
+        console.log(`Rep number ${reps}`)
+    }
 }
---
+++ js-fundamentals-2/while-loop.js
@@ -0,0 +1,11 @@
+// While loop
+/*let rep = 1;
+while (rep <= 10) {
+    console.log(`From While:Rep number ${rep}`)
+    rep++
+}
+
+let dice = Math.trunc(Math.random() * 6 + 1)
+while (dice !== 6) {
+    console.log(`you rolled a ${dice}`)
+}*/
