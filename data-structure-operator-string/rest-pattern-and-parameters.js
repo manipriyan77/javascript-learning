b0e9f0bc5ae069bf5effb3ff1d76604ce8f79ad5
manipriyan77 <manipriyangopalan@gmail.com>
2022-06-29 11:02:14 +0530

    finished rest and spread operators

---
+++ data-structure-operator-string/rest-pattern-and-parameters.js
@@ -0,0 +1,62 @@
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (startedIndex, mainIndex) {
+    return [this.starterMenu[startedIndex], this.mainMenu[mainIndex]];
+  },
+  orderPasta: function (inc1, inc2, inc3) {
+    console.log(`Here is your delicious pasta ${inc1} ${inc2} ${inc3}`);
+  },
+  orderPizza: function (mainIncredient, ...otherIncredients) {
+    console.log(mainIncredient, otherIncredients);
+  },
+};
+// DESTUCTURING
+// spread bcz on right sign of the assignment operator
+const arr = [1, 2, ...[3, 4]];
+
+// syntax for rest bcz on left side of the equal sign
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , Risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, Risotto, otherFood); //Resst elemrnt must be the last element and it can contain only one rest operators
+
+// objects
+const { sat, ...otherDays } = restaurant.openingHours;
+console.log(otherDays);
+
+//FUNCTIONS
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+  }
+  console.log(sum);
+};
+add(1, 2, 3);
+add(1, 2, 23);
+add(1, 2, 3, 7, 8);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushtooms", "olives", "chicken", "tomatos");
--- data-structure-operator-string/spread-operator.js
+++ data-structure-operator-string/spread-operator.js
@@ -1,3 +1,32 @@
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (startedIndex, mainIndex) {
+    return [this.starterMenu[startedIndex], this.mainMenu[mainIndex]];
+  },
+  orderPasta: function (inc1, inc2, inc3) {
+    console.log(`Here is your delicious pasta ${inc1} ${inc2} ${inc3}`);
+  },
+};
+
 const arr = [7, 8, 9];
 
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -7,3 +36,30 @@ console.log(badNewArr);
 const newArr = [1, 2, ...arr];
 console.log(newArr);
 console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, "Dosa"];
+console.log(newMenu);
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+// join 2 arrays or more
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// Iterables are arrays,strings,maps,set except object
+const str = "Manipriyan";
+const letters = [...str, "", "s"];
+console.log(letters);
+
+// const incredients = [
+//   prompt("Let's make pasta   Incredient 1? "),
+//   prompt("Let's make pasta   Incredient 2? "),
+//   prompt("Let's make pasta   Incredient 3? "),
+// ];
+// console.log(incredients);
+
+// restaurant.orderPasta(incredients[0], incredients[1], incredients[2]);
+// restaurant.orderPasta(...incredients);
+
+// spread also works on objects
+const newRestaurant = { ...restaurant, founder: "Manipriyan" };
+console.log(newRestaurant);
